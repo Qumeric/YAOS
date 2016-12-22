4de7048c41d03a7eb8a1f6cede3390979e14d555
Qumeric <qumidium@gmail.com>
2016-12-22 18:32:10 +0300

    Task 3 sample code

--- src/serial.c
+++ src/serial.c
@@ -1,45 +1,47 @@
-#include <ioport.h>
-#include <helpers.h>
 #include <serial.h>
-#include <stdint.h>
+#include <ioport.h>
 
-void init_serial_port() {
-    // Disable interruptions
-    out8(SP_ADRESS + 1, 0);
+#define SERIAL_BASE_PORT	0x3f8
+#define SERIAL_PORT(x)		(SERIAL_BASE_PORT + (x))
+#define REG_DATA		SERIAL_PORT(0)
+#define REG_DLL			SERIAL_PORT(0)
+#define REG_IER			SERIAL_PORT(1)
+#define REG_DLH			SERIAL_PORT(1)
+#define REG_LCR			SERIAL_PORT(3)
+#define REG_LSR			SERIAL_PORT(5)
 
-    // Get ready to recieve a divisor (set DLAB)
-    out8(SP_ADRESS + 3, bit(7));
+#define LCR_8BIT		(3 << 0)
+#define LCR_DLAB		(1 << 7)
+#define LSR_TX_READY		(1 << 5)
 
-    // Set divisor
-    out8(SP_ADRESS + 0, BYTE0(SP_DIVISOR));
-    out8(SP_ADRESS + 1, BYTE1(SP_DIVISOR));
 
-    // Set frame format
-    uint8_t frame_format = 0;
-    if (SP_FRAME_SIZE % 2 == 0)
-        frame_format |= bit(0);
-    if (SP_FRAME_SIZE >= 7)
-        frame_format |= bit(1);
-
-    if (SP_STOP_BITS > 1) // if 3rd bit is 0 then use only 1 stop bit
-        frame_format |= bit(2);
-
-    out8(SP_ADRESS + 3, frame_format);
-
-    // FIXME enable IRQs, set RTS/DSR (http://wiki.osdev.org/Serial_Ports)
-
-    sp_write_string("Serial Port has been initialized!\n");
+void serial_setup(void)
+{
+	/* Step by step explanation:
+	 *   1. i want to disable interrupt, in order to do that i need access
+	 *      to the IER, before writing IER we need to drop DLAB bit in the
+	 *      LCR, so at first i write 0 to LCR, and then write 0 to IER; */
+	out8(REG_LCR, 0);
+	out8(REG_IER, 0);
+	/*   2. now i want to set speed, let say, to 9600 baud,
+	 *      115200/9600 = 12 - is our divisor, but before setting divisor,
+	 *      we need to set DLAB bit in the LCR; */
+	out8(REG_LCR, LCR_DLAB);
+	out8(REG_DLL, 0x0C);
+	out8(REG_DLH, 0x00);
+	/*   3. finally i want to set frame format, there is no tricky parts
+	 *      here. */
+	out8(REG_LCR, LCR_8BIT);
 }
 
-void sp_write_byte(uint8_t byte) {
-    while (!get_bit(in8(SP_ADRESS + 5), 5))
-        ; // polling
-
-    out8(SP_ADRESS+0, byte);
+void serial_putchar(int c)
+{
+	while (!(in8(REG_LSR) & LSR_TX_READY));
+	out8(REG_DATA, c);
 }
 
-void sp_write_string(const char *str) {
-    for (size_t i = 0; str[i]; i++) {
-        sp_write_byte(str[i]);
-    }
+void serial_write(const char *buf, size_t size)
+{
+	for (size_t i = 0; i != size; ++i)
+		serial_putchar(buf[i]);
 }
